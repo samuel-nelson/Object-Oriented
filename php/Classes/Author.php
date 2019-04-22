23cd2c573f09289e62e7c940b06f967a16377e56
Samuel Nelson <snelson54@cnm.edu>
2019-04-22 00:03:41 -0600

    Added getters and setters

--- php/Classes/Author.php
+++ php/Classes/Author.php
@@ -48,16 +48,191 @@ class author {
 	 **/
 	private $authorUsername;
 
-	public function getauthorId(): Uuid {
+	public function getAuthorId(): Uuid {
+		return ($this->authorId);
+	}
+
+	// BREAK
+
+	/**
+	 * accessor method for author id
+	 *
+	 * @return Uuid value of author id (or null if new Author)
+	 **/
+	public function getAuthorId(): Uuid {
 		return ($this->authorId);
 	}
 	/**
-	 * mutator method for Author id
+	 * mutator method for author id
 	 *
 	 * @param  Uuid| string $newAuthorId value of new author id
 	 * @throws \RangeException if $newAuthorId is not positive
 	 * @throws \TypeError if the author Id is not
 	 **/
+	public function setAuthorId( $newAuthorId): void {
+		try {
+			$uuid = self::validateUuid($newAuthorId);
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			$exceptionType = get_class($exception);
+			throw(new $exceptionType($exception->getMessage(), 0, $exception));
+		}
+		// convert and store the Author id
+		$this->authorId = $uuid;
+	}
+	/**
+	 * accessor method for author activation token
+	 *
+	 * @return string value of the activation token
+	 */
+	public function getAuthorActivationToken() : ?string {
+		return ($this->authorActivationToken);
+	}
+	/**
+	 * mutator method for author activation token
+	 *
+	 * @param string $newAuthorActivationToken
+	 * @throws \InvalidArgumentException  if the token is not a string or insecure
+	 * @throws \RangeException if the token is not exactly 32 characters
+	 * @throws \TypeError if the activation token is not a string
+	 */
+	public function setAuthorActivationToken(?string $newAuthorActivationToken): void {
+		if($newAuthorActivationToken === null) {
+			$this->authorActivationToken = null;
+			return;
+		}
+		$newAuthorActivationToken = strtolower(trim($newAuthorActivationToken));
+		if(ctype_xdigit($newAuthorActivationToken) === false) {
+			throw(new\RangeException("author activation is not valid"));
+		}
+		//make sure author activation token is only 32 characters
+		if(strlen($newAuthorActivationToken) !== 32) {
+			throw(new\RangeException("author activation token has to be 32"));
+		}
+		$this->authorActivationToken = $newAuthorActivationToken;
+	}
+	/**
+	 * accessor method for username
+	 *
+	 * @return string value of username
+	 **/
+	public function getAuthorUsername(): string {
+		return ($this->authorUsername);
+	}
+	/**
+	 * mutator method for username
+	 *
+	 * @param string $newAuthorUsername new value of username
+	 * @throws \InvalidArgumentException if $newAuthorUsername is not a string or insecure
+	 * @throws \RangeException if $newAuthorUsername is > 32 characters
+	 * @throws \TypeError if $newAuthorUsername is not a string
+	 **/
+	public function setAuthorUsername(string $newAuthorUsername) : void {
+		// verify the at handle is secure
+		$newAuthorUsername = trim($newAuthorUsername);
+		$newAuthorUsername = filter_var($newAuthorUsername, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+		if(empty($newAuthorUsername) === true) {
+			throw(new \InvalidArgumentException("Author username is empty or insecure"));
+		}
+		// verify the username will fit in the database
+		if(strlen($newAuthorUsername) > 32) {
+			throw(new \RangeException("author username is too large"));
+		}
+		// store the username
+		$this->authorUsername = $newAuthorUsername;
+	}
+	/**
+	 * accessor method for email
+	 *
+	 * @return string value of email
+	 **/
+	public function getAuthorEmail(): string {
+		return $this->authorEmail;
+	}
+	/**
+	 * mutator method for email
+	 *
+	 * @param string $newAuthorEmail new value of email
+	 * @throws \InvalidArgumentException if $newAuthorEmail is not a valid email or insecure
+	 * @throws \RangeException if $newAuthorEmail is > 128 characters
+	 * @throws \TypeError if $newAuthorEmail is not a string
+	 **/
+	public function setAuthorEmail(string $newAuthorEmail): void {
+		// verify the email is secure
+		$newAuthorEmail = trim($newAuthorEmail);
+		$newAuthorEmail = filter_var($newAuthorEmail, FILTER_VALIDATE_EMAIL);
+		if(empty($newAuthorEmail) === true) {
+			throw(new \InvalidArgumentException("author email is empty or insecure"));
+		}
+		// verify the email will fit in the database
+		if(strlen($newAuthorEmail) > 128) {
+			throw(new \RangeException("author email is too large"));
+		}
+		// store the email
+		$this->authorEmail = $newAuthorEmail;
+	}
+	/**
+	 * accessor method for profileHash
+	 *
+	 * @return string value of hash
+	 */
+	public function getAuthorHash(): string {
+		return $this->authorHash;
+	}
 
-
+	/**
+	 * mutator method for author hash password
+	 *
+	 * @param string $newAuthorHash
+	 * @throws \InvalidArgumentException if the hash is not secure
+	 * @throws \RangeException if the hash is not 128 characters
+	 * @throws \TypeError if author hash is not a string
+	 */
+	public function setAuthorHash(string $newAuthorHash): void {
+		//enforce that the hash is properly formatted
+		$newAuthorHash = trim($newAuthorHash);
+		if(empty($newAuthorHash) === true) {
+			throw(new \InvalidArgumentException("author password hash empty or insecure"));
+		}
+		//enforce the hash is really an Argon hash
+		$authorHashInfo = password_get_info($newAuthorHash);
+		if($AuthorHashInfo["algoName"] !== "argon2i") {
+			throw(new \InvalidArgumentException("author hash is not a valid hash"));
+		}
+		//enforce that the hash is exactly 97 characters.
+		if(strlen($newAuthorHash) !== 97) {
+			throw(new \RangeException("author hash must be 97 characters"));
+		}
+		//store the hash
+		$this->authorHash = $newAuthorHash;
+	}
+	/**
+	 * accessor method for Author Avatar Url
+	 *
+	 * @return string value of Author Avatar Url
+	 **/
+	public function getAuthorAvatarUrl(): string {
+		return $this->authorAvatarUrl;
+	}
+	/**
+	 * mutator method for Author Avatar Url
+	 *
+	 * @param string $newAuthorAvatarUrl new value of Author Avatar Url
+	 * @throws \InvalidArgumentException if $newAuthorAvatarUrl is not a valid Author Avatar Url or insecure
+	 * @throws \RangeException if $newAuthorAvatarUrl is > 255 characters
+	 * @throws \TypeError if $newAuthorAvatarUrl is not a string
+	 **/
+	public function setAuthorAvatarUrl(string $newAuthorAvatarUrl): void {
+		// verify the Author Avatar Url is secure
+		$newAuthorAvatarUrl = trim($newAuthorAvatarUrl);
+		$newAuthorAvatarUrl = filter_var($newAuthorAvatarUrl, FILTER_VALIDATE_URL);
+		if(empty($newAuthorAvatarUrl) === true) {
+			throw(new \InvalidArgumentException("Author Avatar Url is empty or insecure"));
+		}
+		// verify the Author Avatar Url will fit in the database
+		if(strlen($newAuthorAvatarUrl) > 255) {
+			throw(new \RangeException("Author Avatar Url is too large"));
+		}
+		// store the Author Avatar Url
+		$this->authorEmail = $newAuthorAvatarUrl;
+	}
 }
