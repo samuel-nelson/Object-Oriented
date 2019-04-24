47dc0c3a3997556e1c2902c65273cfd6fde0ba34
Samuel Nelson <snelson54@cnm.edu>
2019-04-24 07:32:10 -0600

    Added authorId vardump

--- php/lib/author.php
+++ php/lib/author.php
@@ -3,3 +3,11 @@
 // use the new keyword to call the constructor in the class and add all required parameters
 //  var_dump() the result from the step above
 
+require_once(dirname(__DIR__) . "/vendor/autoload.php");
+require_once(dirname(__DIR__) . "/classes/autoload.php");
+
+use Snelson54\objectOriented\Author;
+
+	$authorId = new Author("68fb073ca8494305b623c8cc061710c6", "google.com", "32e2be907fa24035b1336ec7fc2adae3", "snelson54@cnm.edu", "snelson54", "$argon2i$v=19$m=1024,t=384,p=2$T1B6Ymdqa3FJdmZqaDdqYg$hhyC1jf2WjbgfD8Jp6GZE9Tg3IpsYpXKm2VWYOJq8LA");
+
+var_dump($authorId);
